84316f78369629e809f64235d58f9871e367aec0
mohitbhata12 <mohitbhatia486@gmail.com>
2021-06-10 18:41:32 +0530

    popup

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,12 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         todoAdapter= TodoAdapter(mutableListOf())
+        val btnclickme = findViewById<Button>(R.id.btnAddTodo)
+        val btn = findViewById<Button>(R.id.btnDeleteItems)
 
         rec_viewTodoItems.adapter = todoAdapter
         rec_viewTodoItems.layoutManager = LinearLayoutManager(this)
 
-        btnAddTodo.setOnClickListener{
+        btnclickme.setOnClickListener{
             val todoTitle = edittextTodoTitle.text.toString()
+            Toast.makeText(this@MainActivity, "You added a item.", Toast.LENGTH_SHORT).show()
             if (todoTitle.isNotEmpty()){
                 val todo = Todo(todoTitle)
                 todoAdapter.addTodo(todo)
@@ -26,8 +31,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        btnDeleteItems.setOnClickListener {
+        btn.setOnClickListener {
             todoAdapter.deleteDoneTodo()
+            Toast.makeText(this@MainActivity, "Your item is deleted.", Toast.LENGTH_SHORT).show()
         }
     }
 }
